d0bd421cffb9919a7dba55fc733b193b24044621
Hamza Fareed <h.fareed@gmail.com>
2024-07-01 20:16:49 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,51 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "tfm-infra-state-hamfar"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+  }
+}
+
+
+resource "aws_vpc" "test-infra" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "test-infra"
+  }
+}
+
+variable "public_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.0.1.0/24", "10.0.128.0/24"]
+  description = "CIDR block for Public Subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = list(any)
+  default     = ["10.0.16.0/24", "10.0.144.0/24"]
+  description = "CIDR block for Private Subnet"
+}
